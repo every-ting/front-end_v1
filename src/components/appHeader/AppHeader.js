ca7dbdc1692bef40725cce03c69d8eca65e29845
WOOJINCHO98 <steamedbunwith@gmail.com>
2023-05-26 20:36:34 +0900

    feat : add appHeader, BackButton, modal

---
+++ src/components/appHeader/AppHeader.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import './AppHeader.scss';
+import { Link } from 'react-router-dom';
+import { FiStar } from 'react-icons/fi';
+import { RiListSettingsLine } from 'react-icons/ri';
+import { AiOutlineBell } from 'react-icons/ai';
+import FilterModal from '../modal/filterModal/FilterModal';
+
+const AppHeader = ({ title }) => {
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (isFilterModalOpen) {
+      document.body.style.overflow = 'hidden';
+      setTimeout(() => {
+        setIsFilterModalOpen(false);
+      }, 3000);
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+  }, [isFilterModalOpen]);
+  return (
+    <>
+      {isFilterModalOpen && <FilterModal />}
+
+      <div className="appHeaderContainer">
+        <div className="appHeaderWrapper">
+          <div className="appHeaderTitle">
+            <p className="appHeaderTitleText">{title}</p>
+          </div>
+          <div className="appHeaderMenu">
+            <div
+              className="appHeaderMenuItem"
+              onClick={() => {
+                setIsFilterModalOpen(true);
+              }}
+            >
+              <RiListSettingsLine />
+              {/* <Link to="/setting">
+        <RiListSettingsLine />
+      </Link> */}
+            </div>
+            <div className="appHeaderMenuItem">
+              <Link to="/request">
+                <FiStar />
+              </Link>
+            </div>{' '}
+            <div className="appHeaderMenuItem">
+              <Link to="/alert">
+                <AiOutlineBell />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AppHeader;
